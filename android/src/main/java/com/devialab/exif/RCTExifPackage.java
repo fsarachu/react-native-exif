ed5d980274bfb5e5edc05ee9b6f62210c709b510
Mateo Fernández <mateofd@plusvalialabs.com>
2017-09-05 18:13:16 -0300

    Fixes issue #11

--- android/src/main/java/com/devialab/exif/RCTExifPackage.java
+++ android/src/main/java/com/devialab/exif/RCTExifPackage.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -21,6 +22,11 @@ public class RCTExifPackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
